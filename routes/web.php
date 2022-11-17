d1f85cdaf99090e4f53c66de44b285f2e2ec1c65
Alejandro Lodes <janosoft@gmail.com>
2022-11-16 22:15:15 -0300

    Ajustes Laravel 9

--- routes/web.php
+++ routes/web.php
@@ -22,30 +22,45 @@ use App\Http\Controllers\UsuarioController;
 */
 
 Route::get('/', InicioController::class);
-Route::get('continentes', [ContinenteController::class, 'index']);
-Route::get('continentes/create', [ContinenteController::class, 'create']);
-Route::get('continentes/{continente}', [ContinenteController::class, 'show']);
 
-Route::get('paises', [PaisController::class, 'index']);
-Route::get('paises/create', [PaisController::class, 'create']);
-Route::get('paises/{pais}', [PaisController::class, 'show']);
+Route::controller(ContinenteController::class)->group(function () {
+    Route::get('continentes', 'index');
+    Route::get('continentes/create', 'create');
+    Route::get('continentes/{continente}', 'show');
+});
 
-Route::get('divisiones_politicas', [DivisionPoliticaController::class, 'index']);
-Route::get('divisiones_politicas/create', [DivisionPoliticaController::class, 'create']);
-Route::get('divisiones_politicas/{division_politica}', [DivisionPoliticaController::class, 'show']);
+Route::controller(PaisController::class)->group(function () {
+    Route::get('paises', 'index');
+    Route::get('paises/create', 'create');
+    Route::get('paises/{pais}', 'show');
+});
 
-Route::get('divisiones_politicas_tipos', [DivisionPoliticaTipoController::class, 'index']);
-Route::get('divisiones_politicas_tipos/create', [DivisionPoliticaTipoController::class, 'create']);
-Route::get('divisiones_politicas_tipos/{divisiones_politicas_tipo}', [DivisionPoliticaTipoController::class, 'show']);
+Route::controller(DivisionPoliticaController::class)->group(function () {
+    Route::get('divisiones_politicas', 'index');
+    Route::get('divisiones_politicas/create', 'create');
+    Route::get('divisiones_politicas/{division_politica}', 'show');
+});
 
-Route::get('localidades', [LocalidadController::class, 'index']);
-Route::get('localidades/create', [LocalidadController::class, 'create']);
-Route::get('localidades/{localidad}', [LocalidadController::class, 'show']);
+Route::controller(DivisionPoliticaTipoController::class)->group(function () {
+    Route::get('divisiones_politicas_tipos', 'index');
+    Route::get('divisiones_politicas_tipos/create', 'create');
+    Route::get('divisiones_politicas_tipos/{divisiones_politicas_tipo}', 'show');
+});
 
-Route::get('personas', [PersonaController::class, 'index']);
-Route::get('personas/create', [PersonaController::class, 'create']);
-Route::get('personas/{persona}', [PersonaController::class, 'show']);
+Route::controller(LocalidadController::class)->group(function () {
+    Route::get('localidades', 'index');
+    Route::get('localidades/create', 'create');
+    Route::get('localidades/{localidad}', 'show');
+});
 
-Route::get('usuarios', [UsuarioController::class, 'index']);
-Route::get('usuarios/create', [UsuarioController::class, 'create']);
-Route::get('usuarios/{usuario}', [UsuarioController::class, 'show']);
+Route::controller(PersonaController::class)->group(function () {
+    Route::get('personas', 'index');
+    Route::get('personas/create', 'create');
+    Route::get('personas/{persona}', 'show');
+});
+
+Route::controller(UsuarioController::class)->group(function () {
+    Route::get('usuarios', 'index');
+    Route::get('usuarios/create', 'create');
+    Route::get('usuarios/{persona}', 'show');
+});
