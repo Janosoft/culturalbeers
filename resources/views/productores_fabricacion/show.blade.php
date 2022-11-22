1983eaf2f7e12c89c21cbb4adc5503acfa85c259
Alejandro Lodes <janosoft@gmail.com>
2022-11-21 21:54:25 -0300

    Agregando rutas

--- resources/views/productores_fabricacion/show.blade.php
+++ resources/views/productores_fabricacion/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.plantilla')
-@section('title', 'Mostrar Tipo de Fabricación: ' . $cervezas_fabricacion)
+@section('title', 'Mostrar Tipo de Fabricación: ' . $productores_fabricacion)
 
 @section('content')
-    <h1>Contenido de {{ $cervezas_fabricacion }}</h1>
+    <h1>Contenido de {{ $productores_fabricacion }}</h1>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,14 @@ use App\Http\Controllers\DivisionPoliticaTipoController;
 use App\Http\Controllers\LocalidadController;
 use App\Http\Controllers\PersonaController;
 use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\CervezaController;
+use App\Http\Controllers\CervezasColorController;
+use App\Http\Controllers\CervezasEnvaseController;
+use App\Http\Controllers\CervezasEstiloController;
+use App\Http\Controllers\CervezasFamiliaController;
+use App\Http\Controllers\CervezasFermentoController;
+use App\Http\Controllers\ProductorController;
+use App\Http\Controllers\ProductoresFabricacionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -62,5 +70,54 @@ Route::controller(PersonaController::class)->group(function () {
 Route::controller(UsuarioController::class)->group(function () {
     Route::get('usuarios', 'index')->name('usuarios.index');
     Route::get('usuarios/create', 'create')->name('usuarios.create');
-    Route::get('usuarios/{persona}', 'show')->name('usuarios.show');
+    Route::get('usuarios/{usuario}', 'show')->name('usuarios.show');
+});
+
+Route::controller(CervezaController::class)->group(function () {
+    Route::get('cervezas', 'index')->name('cervezas.index');
+    Route::get('cervezas/create', 'create')->name('cervezas.create');
+    Route::get('cervezas/{cerveza}', 'show')->name('cervezas.show');
+});
+
+Route::controller(CervezasColorController::class)->group(function () {
+    Route::get('cervezas_colores', 'index')->name('cervezas_colores.index');
+    Route::get('cervezas_colores/create', 'create')->name('cervezas_colores.create');
+    Route::get('cervezas_colores/{cervezas_color}', 'show')->name('cervezas_colores.show');
+});
+
+Route::controller(CervezasEnvaseController::class)->group(function () {
+    Route::get('cervezas_envases', 'index')->name('cervezas_envases.index');
+    Route::get('cervezas_envases/create', 'create')->name('cervezas_envases.create');
+    Route::get('cervezas_envases/{cervezas_envase}', 'show')->name('cervezas_envases.show');
+});
+
+Route::controller(CervezasEstiloController::class)->group(function () {
+    Route::get('cervezas_estilos', 'index')->name('cervezas_estilos.index');
+    Route::get('cervezas_estilos/create', 'create')->name('cervezas_estilos.create');
+    Route::get('cervezas_estilos/{cervezas_estilo}', 'show')->name('cervezas_estilos.show');
+});
+
+Route::controller(CervezasFamiliaController::class)->group(function () {
+    Route::get('cervezas_familias', 'index')->name('cervezas_familias.index');
+    Route::get('cervezas_familias/create', 'create')->name('cervezas_familias.create');
+    Route::get('cervezas_familias/{cervezas_familia}', 'show')->name('cervezas_familias.show');
+});
+
+Route::controller(CervezasFermentoController::class)->group(function () {
+    Route::get('cervezas_fermentos', 'index')->name('cervezas_fermentos.index');
+    Route::get('cervezas_fermentos/create', 'create')->name('cervezas_fermentos.create');
+    Route::get('cervezas_fermentos/{cervezas_fermento}', 'show')->name('cervezas_fermentos.show');
+});
+
+Route::controller(ProductorController::class)->group(function () {
+    Route::get('productores', 'index')->name('productores.index');
+    Route::get('productores/create', 'create')->name('productores.create');
+    Route::get('productores/{productor}', 'show')->name('productores.show');
+});
+
+
+Route::controller(ProductoresFabricacionController::class)->group(function () {
+    Route::get('productores_fabricaciones', 'index')->name('productores_fabricaciones.index');
+    Route::get('productores_fabricaciones/create', 'create')->name('productores_fabricaciones.create');
+    Route::get('productores_fabricaciones/{productores_fabricacion}', 'show')->name('productores_fabricaciones.show');
 });
