a4586f460d930d1ed80b0f63fb7bd383524fcd99
Alejandro Lodes <janosoft@gmail.com>
2023-01-30 22:17:27 -0300

    lazy load

--- resources/views/components/cerveza.blade.php
+++ resources/views/components/cerveza.blade.php
@@ -4,7 +4,7 @@
             <div class="card-body">
                 <div class="row">
                     <div class="col-3">
-                        <img class="img-fluid rounded-circle" src="{{$src}}" alt="{{ $nombre }}" loading="lazy" style="width: 50px;height: 50px;object-fit: cover;">
+                        <img class="img-fluid rounded-circle" loading="lazy" src="{{$src}}" alt="{{ $nombre }}" loading="lazy" style="width: 50px;height: 50px;object-fit: cover;">
                     </div>
                     <div class="col">
                         {{ $nombre }}
