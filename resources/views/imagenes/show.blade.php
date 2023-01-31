70a07ab5ba0a405e87e694ada5b6bba651ab0c4b
Alejandro Lodes <janosoft@gmail.com>
2023-01-30 22:06:19 -0300

    url relacionada

--- resources/views/imagenes/show.blade.php
+++ resources/views/imagenes/show.blade.php
@@ -5,11 +5,9 @@
     <div class="row">
         <div class="col">
             <img class="img-fluid" src="{{ Storage::url($imagen->url) }}">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
+            <h4>Fecha: {{date('d-m-Y H:m', strtotime($imagen->created_at));}}</h4>
             <h4>Subida por: <a href="{{ route('usuarios.show', $imagen->usuario) }}">{{ $imagen->usuario->persona->nombre }}</a></h4>
+            <h4>Relacionada a: <a href="{{ URL::route($imagen->imageable->getTable() . '.show', $imagen->imageable) }}">{{ $imagen->imageable->nombre }}</a></h4>
         </div>
     </div>
 @endsection
