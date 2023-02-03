c1e08b5513f3a00d8f27ecc77fffea60fe775d62
Alejandro Lodes <janosoft@gmail.com>
2023-02-03 19:18:54 -0300

    componente form nuevo comentario

---
+++ resources/views/components/formularios/comentario.blade.php
@@ -0,0 +1,8 @@
+<form id="form_comentario" action="{{$action}}" method="POST">
+    @csrf
+
+    <div class="input-group mb-3">
+        <textarea class="form-control" name="comentario" id="comentario" rows="1" placeholder="Nuevo Comentario">{{ old('comentario') }}</textarea>
+        <button class="btn btn-outline-primary" type="submit" title="Comentar"><i class="fa-solid fa-comment-medical"></i></button>
+    </div>
+</form>
