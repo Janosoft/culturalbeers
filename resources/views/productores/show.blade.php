9cf65682a1e4e1bcb518cc030b2441a1d872aebb
Alejandro Lodes <janosoft@gmail.com>
2023-02-07 20:31:12 -0300

    productor verificado

--- resources/views/productores/show.blade.php
+++ resources/views/productores/show.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     <div class="row mb-3">
         <div class="col">
-            <h1>{{ $productor->nombre }} @if ($productor->verificado) <i class="fa-solid fa-circle-check fa-2xs text-warning"></i> @endif</h1>
+            <h1>{{ $productor->nombre }} @if ($productor->verificado) <i class="fa-solid fa-circle-check text-warning"></i> @endif</h1>
             <h2><a href="{{ route('localidades.show', $productor->localidad) }}">{{ $productor->localidad->nombre }}</a></h2>
             <h2><a href="{{ route('productores_fabricaciones.show', $productor->fabricacion) }}">{{ $productor->fabricacion->nombre }}</a></h2>
         </div>
