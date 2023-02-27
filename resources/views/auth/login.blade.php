355b7f8a4e27e654fa9092e11e1931571f6a67f6
Alejandro Lodes <janosoft@gmail.com>
2023-02-27 19:46:54 -0300

    mejoras UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,11 +2,20 @@
 @section('title', 'Inicio de Sesión')
 
 @section('content')
-<form action="{{ route('login') }}" method="POST">
-    @csrf
-    
-    <x-input.email label="Email" name="email" placeholder="Email" :value="old('email')" />
-    <x-input.password label="Contraseña" name="password" placeholder="Contraseña" />
-    <x-input.submit label="Iniciar Sesión" icon="bi bi-box-arrow-in-right" value="Login" />
-</form>
+    <div class="row align-items-center mt-2 mt-lg-4 login">
+        <div class="col-lg p-4 text-center">
+            <img src="/logo_icon.png" class="img-fluid" alt="Logo" id="logo">
+        </div>
+        <div class="col-lg">
+            <form class="mb-3 mt-md-4" action="{{ route('login') }}" method="POST">
+                @csrf
+                <x-input.email label="Email" name="email" placeholder="Email" :value="old('email')" />
+                <x-input.password label="Contraseña" name="password" placeholder="Contraseña" />
+                <p class="small"><a class="text-primary" href="forget-password.html">Recuperar Contraseña</a></p>
+                <div class="d-grid">
+                    <x-input.submit label="Iniciar Sesión" icon="bi bi-box-arrow-in-right" value="Login" />
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,13 +2,19 @@
 @section('title', 'Registrarse')
 
 @section('content')
-<form action="{{ route('register') }}" method="POST">
-    @csrf
-    
-    <x-input.text label="Nombre" name="name" placeholder="Nombre y Apellido" :value="old('name')" />
-    <x-input.email label="Email" name="email" placeholder="Email" :value="old('email')" />
-    <x-input.password label="Contraseña" name="password" placeholder="Contraseña" />
-    <x-input.password label="Repetir Contraseña" name="password_confirmation" placeholder="Repetir Contraseña" />
-    <x-input.submit label="Registrarse" icon="bi bi-box-arrow-in-right" value="Register" />
-</form>
+    <div class="row align-items-center mt-2 mt-lg-4 login">
+        <div class="col-lg p-4 text-center">
+            <img src="/logo_icon.png" class="img-fluid" alt="Logo" id="logo">
+        </div>
+        <div class="col-lg">
+            <form class="mb-3 mt-md-4" action="{{ route('register') }}" method="POST">
+                @csrf
+                <x-input.text label="Nombre" name="name" placeholder="Nombre y Apellido" :value="old('name')" />
+                <x-input.email label="Email" name="email" placeholder="Email" :value="old('email')" />
+                <x-input.password label="Contraseña" name="password" placeholder="Contraseña" />
+                <x-input.password label="Repetir Contraseña" name="password_confirmation" placeholder="Repetir Contraseña" />
+                <x-input.submit label="Registrarse" icon="bi bi-box-arrow-in-right" value="Register" />
+            </form>
+        </div>
+    </div>
 @endsection
